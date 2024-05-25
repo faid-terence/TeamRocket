b1dec10408c12132e7cb51dfd7b5578b1afa899d
Terence Faid JABO <faidterence@outlook.com>
2024-05-25 20:43:50 +0200

    Initialize TeamRocket project with database connection

---
+++ src/test/java/com/faidterence/TeamRocket/TeamRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faidterence.TeamRocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
